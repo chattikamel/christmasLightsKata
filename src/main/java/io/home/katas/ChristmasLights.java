22c511869296bbcef7c44bc1c7b3cba9b88ea1a7
Kamel CHATTI <chattikamel@gmail.com>
2021-02-14 07:26:17 +0100

    fail Christmas lights  creation test

---
+++ src/main/java/io/home/katas/ChristmasLights.java
@@ -0,0 +1,11 @@
+package io.home.katas;
+
+import lombok.Data;
+
+@Data
+public class ChristmasLights {
+
+    Object[][] grid;
+
+
+}
--- src/test/java/io/home/katas/MainTest.java
+++ src/test/java/io/home/katas/MainTest.java
@@ -3,10 +3,26 @@ package io.home.katas;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class MainTest {
 
     @Test
-    void test() {
+    void create_christmasLights_test() {
 
+        ChristmasLights christmasLights = new ChristmasLights();
+
+        assertThat(christmasLights).isNotNull();
+
+    }
+
+    @Test
+    void christmasLights_has_aGridOf_2dim_of_1000_Lights_test() {
+        ChristmasLights christmasLights = new ChristmasLights();
+
+        assertThat(christmasLights.getGrid()).isNotNull();
+        for (int i = 0; i < 1000; i++) {
+            assertThat(christmasLights.getGrid()[i].length).isEqualTo(1000);
+        }
     }
 }
