659bf3a104924732e1c8d1e7f8c229e189e1f077
Kamel CHATTI <chattikamel@gmail.com>
2021-02-14 08:05:24 +0100

    test refactoring

--- src/test/java/io/home/katas/MainTest.java
+++ src/test/java/io/home/katas/MainTest.java
@@ -2,16 +2,23 @@ package io.home.katas;
 
 
 import org.assertj.core.api.Condition;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class MainTest {
 
+    public static final Condition<Light> LIGHT_ON_CONDITION = new Condition<>(Light::isOn, "Light on test");
+    private ChristmasLights christmasLights;
+
+    @BeforeEach
+    void setUp() {
+        christmasLights = new ChristmasLights();
+    }
+
     @Test
     void christmasLights_has_aGridOf_2dim_of_1000_Lights_test() {
-        ChristmasLights christmasLights = new ChristmasLights();
-
         assertThat(christmasLights.getGrid()).isNotNull();
         for (int i = 0; i < 1000; i++) {
             assertThat(christmasLights.getGrid()[i].length)
@@ -21,11 +28,10 @@ class MainTest {
 
     @Test
     void when_christmasLights_turnOn_theLight_in_theSpecificPosition_itShould_lightOn_test() {
-        ChristmasLights christmasLights = new ChristmasLights();
         int x=5, y=6;
         christmasLights.turnOn(x, y);
 
         assertThat(christmasLights.getGrid()[x][y]).is(
-                new Condition<>(Light::isOn, "Light on test"));
+                LIGHT_ON_CONDITION);
     }
 }
