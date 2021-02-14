004faa7677afb201447a1a0c609082d95b006b6c
Kamel CHATTI <chattikamel@gmail.com>
2021-02-14 07:40:10 +0100

    refactor

--- src/test/java/io/home/katas/MainTest.java
+++ src/test/java/io/home/katas/MainTest.java
@@ -8,21 +8,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 class MainTest {
 
     @Test
-    void create_christmasLights_test() {
-
-        ChristmasLights christmasLights = new ChristmasLights();
-
-        assertThat(christmasLights).isNotNull();
-
-    }
-
-    @Test
     void christmasLights_has_aGridOf_2dim_of_1000_Lights_test() {
         ChristmasLights christmasLights = new ChristmasLights();
 
         assertThat(christmasLights.getGrid()).isNotNull();
         for (int i = 0; i < 1000; i++) {
-            assertThat(christmasLights.getGrid()[i].length).isEqualTo(1000);
+            assertThat(christmasLights.getGrid()[i].length)
+                    .isEqualTo(1000);
         }
     }
 }
